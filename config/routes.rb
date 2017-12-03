c589498714537f7724c349d49920381a069b233b
Jiro.Kirameki <hida@triton.ocn.ne.jp>
2017-12-03 15:19:36 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   get 'toppages/index'
 
   get 'sessions/new'
@@ -6,14 +16,21 @@ Rails.application.routes.draw do
   get 'sessions/create'
 
   get 'sessions/destroy'
-
-  root to: "tasks#index"
+  
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
+    root to: "tasks#index"
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :tasks, only: [:index, :show, :new, :create]
-  
-  root to: 'toppages#index'
 end
