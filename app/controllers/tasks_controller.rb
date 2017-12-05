b6ed5614ae2d5d88bd048ccbdd8e7c5fda2d1b96
Jiro.Kirameki <hida@triton.ocn.ne.jp>
2017-12-05 13:53:44 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,9 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all.page(params[:page])
+    if logged_in?
+      #@tasks = Task.all.page(params[:page])
+      @tasks = current_user.tasks.page(params[:page])
+    end
   end
 
   def show
@@ -12,7 +15,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に連絡されました'
@@ -38,7 +42,7 @@ class TasksController < ApplicationController
       render :edit
     end
   end
-
+  
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
@@ -53,5 +57,11 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
-
